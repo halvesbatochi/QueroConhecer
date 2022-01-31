20c91ed67647bf8970111ece5ada2f1f9689838b
Henrique Alves Batochi <32850493+halvesbatochi@users.noreply.github.com>
2022-01-31 14:01:38 -0300

    Adicionado Gesture recognizer por código no mapKit

--- QueroConhecer/QueroConhecer/PlaceFinderViewController.swift
+++ QueroConhecer/QueroConhecer/PlaceFinderViewController.swift
@@ -10,14 +10,44 @@ import MapKit
 
 class PlaceFinderViewController: UIViewController {
     
+    enum PlaceFinderMessageType {
+        case error(String)
+        case confirmation(String)
+    }
+    
     @IBOutlet weak var tfCity: UITextField!
     @IBOutlet weak var mapView: MKMapView!
     @IBOutlet weak var viLoading: UIView!
     @IBOutlet weak var aiLoading: UIActivityIndicatorView!
     
+    var place: Place!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        // Criando Gesture Recognizer - Long Press
+        let gesture = UILongPressGestureRecognizer(target: self, action: #selector(getLocation(_:)))
+        gesture.minimumPressDuration = 2.0
+        mapView.addGestureRecognizer(gesture)
+    }
+    
+    @objc func getLocation(_ gesture: UILongPressGestureRecognizer) {
+        if gesture.state == .began {
+            load(show: true)
+            let point = gesture.location(in: mapView)
+            let coordinate = mapView.convert(point, toCoordinateFrom: mapView)
+            let location = CLLocation(latitude: coordinate.latitude, longitude: coordinate.longitude)
+            CLGeocoder().reverseGeocodeLocation(location, completionHandler: { (placemarks, error) in
+                self.load(show: false)
+                if error == nil {
+                    if !self.savePlace(with: placemarks?.first) {
+                        self.showMessage(type: .error("Não foi encontrado nenhum local com esse nome"))
+                    }
+                } else {
+                    self.showMessage(type: .error("Erro desconhecido"))
+                }
+            })
+        }
     }
     
     @IBAction func findCity(_ sender: Any) {
@@ -28,9 +58,56 @@ class PlaceFinderViewController: UIViewController {
         geoCoder.geocodeAddressString(address) { (placemarks, error) in
             // Como estou dentro de uma clousure preciso do SELF para ter acesso aos metodos
             self.load(show: false)
-            guard let placemark = placemarks?.first else { return }
-            print(Place.getFormattedAddress(with: placemark))
+            if error == nil {
+                if !self.savePlace(with: placemarks?.first) {
+                    self.showMessage(type: .error("Não foi encontrado nenhum local com esse nome"))
+                }
+            } else {
+                self.showMessage(type: .error("Erro desconhecido"))
+            }
         }
+    }
+    
+    func savePlace(with placemark: CLPlacemark?) -> Bool {
+        guard let placemark = placemark, let coordinate = placemark.location?.coordinate else {
+            return false
+        }
+        let name    = placemark.name ?? placemark.country ?? "Desconhecido"
+        let address = Place.getFormattedAddress(with: placemark)
+        place = Place(name: name, latitude: coordinate.latitude, longitude: coordinate.longitude, address: address)
+        
+        let region = MKCoordinateRegion(center: coordinate, latitudinalMeters: 3500, longitudinalMeters: 3500)
+        mapView.setRegion(region, animated: true)
+        
+        self.showMessage(type: .confirmation(place.name))
+        
+        return true
+    }
+    
+    func showMessage(type: PlaceFinderMessageType) {
+        let title: String, message: String
+        var hasConfirmation: Bool = false
+        
+        switch type {
+        case .confirmation(let name):
+            title = "Local encontrado"
+            message = "Deseja adicionar \(name)?"
+            hasConfirmation = true
+        case .error(let errorMessage):
+            title = "Erro"
+            message = errorMessage
+        }
+        
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let cancelAction = UIAlertAction(title: "Cancelar", style: .cancel, handler: nil)
+        alert.addAction(cancelAction)
+        if hasConfirmation {
+            let confirmAction = UIAlertAction(title: "OK", style: .default, handler:  {(action) in
+                print("OK!!!")
+            })
+            alert.addAction(confirmAction)
+        }
+        present(alert, animated: true, completion: nil)
     }
     
     func load(show: Bool) {
